cf509cf647b67499da0b10cee6ef3dab8378d1f9
Arashi <35284190+cd-7@users.noreply.github.com>
2021-12-30 14:46:33 -0800

    bug fixes

--- blackjack.java
+++ blackjack.java
@@ -18,6 +18,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
+            label172:
             while(run) {
                 double dealerCardOne = (double)(Math.round(Math.random() * 9.0D) + 2L);
                 double dealerCardTwo = (double)(Math.round(Math.random() * 9.0D) + 1L);
@@ -27,7 +28,7 @@ public class Main {
                 while(true) {
                     while(playerChips < 10) {
                         int buyInAmount = false;
-                        System.out.println("No more chips.");
+                        System.out.println("Not enough chips.");
                         System.out.print("Buy More? (y, n): ");
                         String buyInAnswer = scanner.next();
                         if (!buyInAnswer.equals("y")) {
@@ -44,132 +45,148 @@ public class Main {
                                     break;
                                 }
 
-                                System.out.println("Please enter a buy-in amount between 10 - 5000.");
+                                System.out.println("Please enter a buy-in amount between 10 - 20000.");
                             }
                         }
                     }
 
-                    System.out.print("Your Bet (10 - 10000): ");
-                    int bettingAmount = scanner.nextInt();
-                    if (bettingAmount >= 10 && bettingAmount <= 10000) {
-                        System.out.println("Dealer Cards: " + (int)dealerCardOne + ", ?");
-                        double playerCards = (double)(Math.round(Math.random() * 9.0D) + 2L + Math.round(Math.random() * 9.0D) + 1L);
-                        System.out.println("You: " + (int)playerCards);
-                        if (dealerCards == 21.0D && playerCards == 21.0D) {
-                            System.out.println("Tie.");
-                        } else if (playerCards == 21.0D) {
-                            System.out.println("You: " + (int)playerCards);
-                            System.out.println("Dealer: " + (int)dealerCards);
-                            System.out.println("Blackjack! You Win!");
-                        } else if (dealerCards == 21.0D) {
-                            System.out.println("You: " + (int)playerCards);
-                            System.out.println("Dealer: " + (int)dealerCards);
-                            System.out.println("Blackjack... You Lose.");
-                        } else {
-                            String playerMove = "y";
+                    while(true) {
+                        System.out.print("Your Bet (10 - 10000): ");
+                        int bettingAmount = scanner.nextInt();
+                        if (bettingAmount >= 10 && bettingAmount <= 15000) {
+                            if (playerChips > bettingAmount) {
+                                System.out.println("Dealer Cards: " + (int)dealerCardOne + ", ?");
+                                double playerCards = (double)(Math.round(Math.random() * 9.0D) + 2L + Math.round(Math.random() * 9.0D) + 1L);
+                                System.out.println("You: " + (int)playerCards);
+                                if (dealerCards == 21.0D && playerCards == 21.0D) {
+                                    System.out.println("Tie.");
+                                } else if (playerCards == 21.0D) {
+                                    System.out.println("You: " + (int)playerCards);
+                                    System.out.println("Dealer: " + (int)dealerCards);
+                                    System.out.println("Blackjack! You Win!");
+                                } else if (dealerCards == 21.0D) {
+                                    System.out.println("You: " + (int)playerCards);
+                                    System.out.println("Dealer: " + (int)dealerCards);
+                                    System.out.println("Blackjack... You Lose.");
+                                } else {
+                                    String playerMove = "y";
 
-                            label144:
-                            while(true) {
-                                label142:
-                                while(true) {
-                                    if (!playerMove.equals("y") || !(playerCards < 21.0D)) {
-                                        break label144;
-                                    }
-
+                                    label161:
                                     while(true) {
+                                        label159:
                                         while(true) {
-                                            System.out.print("Hit? (y, n, dd): ");
-                                            playerMove = scanner.next().toLowerCase();
-                                            if (!playerMove.equals("y") && !playerMove.equals("n") && !playerMove.equals("dd")) {
-                                                System.out.println("Please enter y, n, or dd.");
-                                            } else {
-                                                if (playerMove.equals("dd") && playerChips > bettingAmount * 2 || playerMove.equals("y") || playerMove.equals("n")) {
-                                                    if (playerMove.equals("y") && playerCards < 21.0D) {
-                                                        playerCards = playerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
-                                                        System.out.println("You: " + (int)playerCards);
-                                                        continue label142;
-                                                    }
+                                            if (!playerMove.equals("y") || !(playerCards < 21.0D)) {
+                                                break label161;
+                                            }
 
-                                                    if (playerMove.equals("dd") && playerCards < 21.0D && playerChips > bettingAmount * 2) {
-                                                        bettingAmount *= 2;
-                                                        playerCards = playerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
-                                                        System.out.println("You: " + (int)playerCards);
+                                            while(true) {
+                                                while(true) {
+                                                    System.out.print("Hit? (y, n, dd): ");
+                                                    playerMove = scanner.next().toLowerCase();
+                                                    if (!playerMove.equals("y") && !playerMove.equals("n") && !playerMove.equals("dd")) {
+                                                        System.out.println("Please enter y, n, or dd.");
+                                                    } else {
+                                                        if (playerMove.equals("dd") && playerChips >= bettingAmount * 2 || playerMove.equals("y") || playerMove.equals("n")) {
+                                                            if (playerMove.equals("y") && playerCards < 21.0D) {
+                                                                playerCards = playerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
+                                                                System.out.println("You: " + (int)playerCards);
+                                                                if (playerCards == 21.0D) {
+                                                                    while(true) {
+                                                                        if (!(dealerCards < 17.0D)) {
+                                                                            break label161;
+                                                                        }
 
-                                                        while(true) {
-                                                            if (!(dealerCards < 17.0D)) {
-                                                                break label144;
+                                                                        dealerCards = dealerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
+                                                                    }
+                                                                }
+                                                                continue label159;
                                                             }
 
-                                                            dealerCards = dealerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
-                                                        }
-                                                    }
+                                                            if (playerMove.equals("dd") && playerCards < 21.0D && playerChips >= bettingAmount * 2) {
+                                                                bettingAmount *= 2;
+                                                                playerCards = playerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
+                                                                System.out.println("You: " + (int)playerCards);
 
-                                                    if (playerMove.equals("n")) {
-                                                        System.out.println("You: " + (int)playerCards);
+                                                                while(true) {
+                                                                    if (!(dealerCards < 17.0D)) {
+                                                                        break label161;
+                                                                    }
 
-                                                        while(true) {
-                                                            if (!(dealerCards < 17.0D)) {
-                                                                break label144;
+                                                                    dealerCards = dealerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
+                                                                }
                                                             }
 
-                                                            dealerCards = dealerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
-                                                        }
-                                                    }
+                                                            if (playerMove.equals("n")) {
+                                                                System.out.println("You: " + (int)playerCards);
 
-                                                    if (playerMove.equals("q")) {
-                                                        System.exit(0);
+                                                                while(true) {
+                                                                    if (!(dealerCards < 17.0D)) {
+                                                                        break label161;
+                                                                    }
+
+                                                                    dealerCards = dealerCards + (double)Math.round(Math.random() * 8.0D) + 2.0D;
+                                                                }
+                                                            }
+
+                                                            if (playerMove.equals("q")) {
+                                                                System.exit(0);
+                                                            }
+                                                            continue label159;
+                                                        }
+
+                                                        System.out.println("Not enough chips to double down.");
                                                     }
-                                                    continue label142;
                                                 }
-
-                                                System.out.println("Not enough chips to double down.");
                                             }
                                         }
                                     }
                                 }
-                            }
-                        }
 
-                        if (playerCards > 21.0D) {
-                            playerChips -= bettingAmount;
-                            System.out.println("Bust. You Lose.");
-                            System.out.println("-" + bettingAmount + " Chips");
-                            System.out.println();
-                            System.out.println("Your Chips: " + playerChips);
+                                if (playerCards > 21.0D) {
+                                    playerChips -= bettingAmount;
+                                    System.out.println("Bust. You Lose.");
+                                    System.out.println("-" + bettingAmount + " Chips");
+                                    System.out.println();
+                                    System.out.println("Your Chips: " + playerChips);
+                                    continue label172;
+                                }
+
+                                if (dealerCards > 21.0D) {
+                                    playerChips += bettingAmount;
+                                    System.out.println("Dealer: " + (int)dealerCards);
+                                    System.out.println("Dealer Busts. You Win!");
+                                    System.out.println("+" + bettingAmount + " Chips");
+                                    System.out.println();
+                                    System.out.println("Your Chips: " + playerChips);
+                                } else if (playerCards > dealerCards && playerCards <= 21.0D) {
+                                    playerChips += bettingAmount;
+                                    System.out.println("Dealer: " + (int)dealerCards);
+                                    System.out.println("You Win!");
+                                    System.out.println("+" + bettingAmount + " Chips");
+                                    System.out.println();
+                                    System.out.println("Your Chips: " + playerChips);
+                                } else if (playerCards < dealerCards) {
+                                    playerChips -= bettingAmount;
+                                    System.out.println("Dealer: " + (int)dealerCards);
+                                    System.out.println("You Lose.");
+                                    System.out.println("-" + bettingAmount + " Chips");
+                                    System.out.println();
+                                    System.out.println("Your Chips: " + playerChips);
+                                } else {
+                                    System.out.println("Dealer: " + (int)dealerCards);
+                                    System.out.println("Tie!");
+                                    System.out.println();
+                                    System.out.println("Your Chips: " + playerChips);
+                                }
+                                continue label172;
+                            }
+
+                            System.out.println("Not enough chips.");
                             break;
                         }
 
-                        if (dealerCards > 21.0D) {
-                            playerChips += bettingAmount;
-                            System.out.println("Dealer: " + (int)dealerCards);
-                            System.out.println("Dealer Busts. You Win!");
-                            System.out.println("+" + bettingAmount + " Chips");
-                            System.out.println();
-                            System.out.println("Your Chips: " + playerChips);
-                        } else if (playerCards > dealerCards && playerCards <= 21.0D) {
-                            playerChips += bettingAmount;
-                            System.out.println("Dealer: " + (int)dealerCards);
-                            System.out.println("You Win!");
-                            System.out.println("+" + bettingAmount + " Chips");
-                            System.out.println();
-                            System.out.println("Your Chips: " + playerChips);
-                        } else if (playerCards < dealerCards) {
-                            playerChips -= bettingAmount;
-                            System.out.println("Dealer: " + (int)dealerCards);
-                            System.out.println("You Lose.");
-                            System.out.println("-" + bettingAmount + " Chips");
-                            System.out.println();
-                            System.out.println("Your Chips: " + playerChips);
-                        } else {
-                            System.out.println("Dealer: " + (int)dealerCards);
-                            System.out.println("Tie!");
-                            System.out.println();
-                            System.out.println("Your Chips: " + playerChips);
-                        }
-                        break;
+                        System.out.println("Please enter a bet between 10 and 10000.");
                     }
-
-                    System.out.println("Please enter a bet between 10 and 10000.");
                 }
             }
 
